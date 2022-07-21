1a5a96b3ff30260166a654c08ee782819da49641
vishant <vishant.96.sehrawat@gmail.com>
2022-07-22 01:53:36 +0530

    completed without responsiveness

---
+++ homepage.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 2,
+    direction: getDirection(),
+    navigation: {
+        nextEl: ".next",
+        prevEl: ".prev",
+    },
+    on: {
+        resize: function () {
+            swiper.changeDirection(getDirection());
+        },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
+
+    return direction;
+}
